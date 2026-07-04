wazero/wazero

arm64: IRem result reg may contain garbage after the exit sequence (#2508)
This is an extremely subtle bug in arm64 codegen that may manifest as a
crash or an infinite loop depending on the binary, as reported by @Xe in
Anubis.

The fix however is quite simple: `lowerIRem()` needs to store the result
register `rd` to a `tmp` register, before the exit sequence; otherwise
the result _might_ be overwritten (e.g. by a spill reload) across the
basic block boundary (the exit sequence introduces a basic block).

Because this only happens under some **register pressure** this bug may
not present itself very often: in fact, the **reproducer is relatively
big**.

The garbage value in the register will then result in any sort of weird
behavior, especially if that register is then used as a sentinel or a
target for a jump.

In the past we had to deal with it already: one example is in
`lowerFpuToInt()` where both `ctx` and `rn` are copied to temp
registers.

Signed-off-by: Edoardo Vacchi <evacchi@users.noreply.github.com>

internal/integration_test/engine/urem_regalloc_test.go
@@ -1,0 +1,52 @@
+package adhoc
+
+import (
+	"context"
+	_ "embed"
+	"encoding/binary"
+	"testing"
+
+	"github.com/tetratelabs/wazero"
+	"github.com/tetratelabs/wazero/api"
+	"github.com/tetratelabs/wazero/internal/testing/require"
+)
+
+//go:embed testdata/urem_regalloc.wasm
+var uremRegallocWasm []byte
+
+// TestUremRegalloc is a regression test for an ARM64 compiler bug where the
+// register allocator inserts a spill/reload between the udiv and msub
+// instructions that implement i32.rem_u, clobbering the quotient register.
+// This causes the remainder to be computed with a stale value, producing
+// a wrong result that triggers a spurious bounds-check unreachable trap.
+func TestUremRegalloc(t *testing.T) {
+	ctx := context.Background()
+	r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig())
+	defer r.Close(ctx)
+
+	_, err := r.NewHostModuleBuilder("repro").
+		NewFunctionBuilder().WithFunc(func(context.Context, uint32) {}).
+		Export("update_nonce").
+		Instantiate(ctx)
+	require.NoError(t, err)
+
+	mod, err := r.Instantiate(ctx, uremRegallocWasm)
+	require.NoError(t, err)
+	defer mod.Close(ctx)
+
+	_, ok := mod.Memory().Grow(300)
+	require.True(t, ok)
+
+	self := make([]byte, 32)
+	binary.LittleEndian.PutUint32(self[8:], 8)
+	binary.LittleEndian.PutUint32(self[12:], 2)
+	binary.LittleEndian.PutUint32(self[16:], 1)
+	ok = mod.Memory().Write(0x100000, self)
+	require.True(t, ok)
+
+	mod.ExportedGlobal("__stack_pointer").(api.MutableGlobal).Set(0xFFF00)
+
+	result, err := mod.ExportedFunction("fill_blocks").Call(ctx, 0x100000, 0x120000, 8, 0xFFF00)
+	require.NoError(t, err)
+	require.Equal(t, uint64(18), result[0])
+}

mgilbir/andsifr

(cherry picked from commit 89079b9166ffc1e1fbebabc1560aec7ddbc2c224)

internal/integration_test/engine/urem_regalloc_test.go
@@ -1,0 +1,52 @@
+package adhoc
+
+import (
+	"context"
+	_ "embed"
+	"encoding/binary"
+	"testing"
+
+	"github.com/mgilbir/andsifr"
+	"github.com/mgilbir/andsifr/api"
+	"github.com/mgilbir/andsifr/internal/testing/require"
+)
+
+//go:embed testdata/urem_regalloc.wasm
+var uremRegallocWasm []byte
+
+// TestUremRegalloc is a regression test for an ARM64 compiler bug where the
+// register allocator inserts a spill/reload between the udiv and msub
+// instructions that implement i32.rem_u, clobbering the quotient register.
+// This causes the remainder to be computed with a stale value, producing
+// a wrong result that triggers a spurious bounds-check unreachable trap.
+func TestUremRegalloc(t *testing.T) {
+	ctx := context.Background()
+	r := wazero.NewRuntimeWithConfig(ctx, wazero.NewRuntimeConfig())
+	defer r.Close(ctx)
+
+	_, err := r.NewHostModuleBuilder("repro").
+		NewFunctionBuilder().WithFunc(func(context.Context, uint32) {}).
+		Export("update_nonce").
+		Instantiate(ctx)
+	require.NoError(t, err)
+
+	mod, err := r.Instantiate(ctx, uremRegallocWasm)
+	require.NoError(t, err)
+	defer mod.Close(ctx)
+
+	_, ok := mod.Memory().Grow(300)
+	require.True(t, ok)
+
+	self := make([]byte, 32)
+	binary.LittleEndian.PutUint32(self[8:], 8)
+	binary.LittleEndian.PutUint32(self[12:], 2)
+	binary.LittleEndian.PutUint32(self[16:], 1)
+	ok = mod.Memory().Write(0x100000, self)
+	require.True(t, ok)
+
+	mod.ExportedGlobal("__stack_pointer").(api.MutableGlobal).Set(0xFFF00)
+
+	result, err := mod.ExportedFunction("fill_blocks").Call(ctx, 0x100000, 0x120000, 8, 0xFFF00)
+	require.NoError(t, err)
+	require.Equal(t, uint64(18), result[0])
+}